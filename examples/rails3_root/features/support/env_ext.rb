f49ee1cf93e4b535e3ae93bf260b56c1394dc711
George Ardeleanu <gardelea@gmail.com>
2012-04-29 10:59:03 -0400

    removing warnings like "rack-1.2.5/lib/rack/utils.rb:16: warning: regexp match /.../n against to UTF-8 string"

--- examples/rails3_root/features/support/env_ext.rb
+++ examples/rails3_root/features/support/env_ext.rb
@@ -1,2 +1,6 @@
 # email testing in cucumber
 require 'email_spec/cucumber'
+
+# patch to remove warnings like this :
+# savingstar-web/vendor/bundle/ruby/1.9.1/gems/rack-1.2.5/lib/rack/utils.rb:16: warning: regexp match /.../n against to UTF-8 string
+require 'rack_util_warnings_patch'
